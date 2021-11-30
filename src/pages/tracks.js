1e67f232b3f0bd2189c59815c89ca7f3238d54c0
Nikkels <rnikkels@bol.com>
2021-11-30 18:07:50 +0100

    replaced length with durationInSeconds

--- src/pages/tracks.js
+++ src/pages/tracks.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { useQuery, gql } from '@apollo/client';
+import {gql, useQuery} from '@apollo/client';
 import TrackCard from '../containers/track-card';
-import { Layout, QueryResult } from '../components';
+import {Layout, QueryResult} from '../components';
 
 /** TRACKS gql query to retreive all tracks */
 export const TRACKS = gql`
-  query getTracks {
-    tracksForHome {
-      id
-      title
-      thumbnail
-      length
-      modulesCount
-      author {
-        name
-        photo
-      }
+    query getTracks {
+        tracksForHome {
+            id
+            title
+            thumbnail
+            durationInSeconds
+            modulesCount
+            author {
+                name
+                photo
+            }
+        }
     }
-  }
 `;
 
 /**
@@ -25,13 +25,13 @@ export const TRACKS = gql`
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  const { loading, error, data } = useQuery(TRACKS);
+  const {loading, error, data} = useQuery(TRACKS);
 
   return (
     <Layout grid>
       <QueryResult error={error} loading={loading} data={data}>
         {data?.tracksForHome?.map((track, index) => (
-          <TrackCard key={track.id} track={track} />
+          <TrackCard key={track.id} track={track}/>
         ))}
       </QueryResult>
     </Layout>
